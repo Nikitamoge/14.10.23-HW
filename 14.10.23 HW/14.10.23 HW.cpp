a60f52f0fdb2a4d9bc2cc706f5e59af85eb5750f
Nikita Iliuschchenko <Nike.il.od@gmail.com>
2023-10-18 12:44:16 +0200

    Add project files.

---
+++ 14.10.23 HW/14.10.23 HW.cpp
@@ -0,0 +1,48 @@
+﻿#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+//Task 1 func
+void replacing(char* str) {
+    int length = strlen(str);
+
+    for (int i = 0; i < length; i++) {
+        if (str[i] == '.') {
+            str[i] = '!';
+        }
+    }
+}
+
+//Task 2 func
+int counter(char* str, char c) {
+    int count = 0;
+    for (int i = 0; i < strlen(str); i++) {
+        if (str[i] == c) {
+            count++;
+        }
+    }
+    return count;
+}
+
+int main() {
+    //Task 1
+    char str[100];
+
+    cout << "Enter the string: ";
+    cin.getline(str, sizeof(str));
+
+    replacing(str);
+    cout << "Results: " << str << endl;
+
+    //Task 2
+    char str[] = "Symbols counter";
+    char c;
+
+    cout << "Enter symbols: ";
+    cin >> c;
+
+    cout << "Symbol '" << c << "' appears in the string '" << str << "' " << counter(str, c) << " times." << endl;
+    return 0;
+}
+
